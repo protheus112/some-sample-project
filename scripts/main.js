23214c7261f495f1f1a80c6dca1e3853b887f513
Catalin Costinean <catalin.costinean7@gmail.com>
2020-04-30 21:34:27 +0300

    Changed pic, modified the index html, added css and js

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const myHeading = document.querySelector('h1'); // declaring the h1 in a variable
+//myHeading.textContent = "Hello world!"; //fetching the variable, declaring it's value
+/* comment on multiple lines
+like in the case of css and html */
+let firstTest = 'vanilla';
+if(firstTest === 'vanilla'){
+    myHeading.textContent = "Test OK";
+    
+} else {
+    myHeading.textContent = "Not OK"
+}
